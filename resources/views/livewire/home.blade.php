9e7ba0f2aadc0c7dc78baf68ab48a059770ed0a2
Heru Firmansyah <heru0502@gmail.com>
2021-06-02 01:34:17 +0800

    Added slider on home page

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -7,7 +7,41 @@
       <input type="text" class="border-0" placeholder="Apa yang dicari hari ini?">
     </div>
 
-    <div class="card card-style">
+    <div class="splide single-slider slider-arrows" wire:ignore id="single-slider-3">
+      <div class="splide__track">
+        <div class="splide__list">
+          <div class="splide__slide">
+            <div data-card-height="200" class="card mx-3 bg-18 rounded-m shadow-l">
+              <div class="card-bottom text-center mb-3">
+                <h2 class="color-white text-uppercase font-900 mb-0">Splendid Simplicity</h2>
+                <p class="under-heading color-white">A new generation of Mobile Kits.</p>
+              </div>
+              <div class="card-overlay bg-gradient"></div>
+            </div>
+          </div>
+          <div class="splide__slide">
+            <div data-card-height="200" class="card mx-3 bg-14 rounded-m shadow-l">
+              <div class="card-bottom text-center mb-3">
+                <h2 class="color-white text-uppercase font-900 mb-0">Top Notch Quality</h2>
+                <p class="under-heading color-white">Flexibility, Speed, Ease of Use.</p>
+              </div>
+              <div class="card-overlay bg-gradient"></div>
+            </div>
+          </div>
+          <div class="splide__slide">
+            <div data-card-height="200" class="card mx-3 bg-14 rounded-m shadow-l">
+              <div class="card-bottom text-center mb-3">
+                <h2 class="color-white text-uppercase font-900 mb-0">Perfectly Organized</h2>
+                <p class="under-heading color-white">Mobile Website, or App or PWA Ready.</p>
+              </div>
+              <div class="card-overlay bg-gradient"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="card card-style mt-3">
       <div class="content">
         <div class="row mb-0 text-center">
           <a href="#" class="col-3 align-text-bottom">
