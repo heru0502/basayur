3a6c975bab6a8329d11c3c23e61cf3b7daf23b24
Heru Firmansyah <heru0502@gmail.com>
2021-06-13 15:16:13 +0800

    Add function trait WithCart, WithModalmenu in list menu

--- app/Http/Livewire/MenuIndex.php
+++ app/Http/Livewire/MenuIndex.php
@@ -2,13 +2,22 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Menu;
+use App\Models\MenuCategory;
+use App\Traits\WithCart;
+use App\Traits\WithModalMenu;
 use Livewire\Component;
 
 class MenuIndex extends Component
 {
+    use WithCart, WithModalMenu;
+
     public function render()
     {
-        return view('livewire.menu-index')
+        $categories = MenuCategory::all();
+        $menus = Menu::with('unit')->latest()->get();
+
+        return view('livewire.menu-index', compact('categories', 'menus'))
             ->layout('layouts.customer');
     }
 }
--- resources/views/livewire/menu-index.blade.php
+++ resources/views/livewire/menu-index.blade.php
@@ -2,9 +2,9 @@
   <div class="header header-fixed header-logo-center">
     <a href="index.html" class="header-title">Pencarian</a>
     <a href="#" data-back-button class="header-icon header-icon-1"><i class="fas fa-arrow-left"></i></a>
-    <a href="/welcome/skip" class="header-icon header-icon-4">
+    <a href="/cart" class="header-icon header-icon-4">
       <i class="fa fa-shopping-basket"></i>
-      <span class="badge bg-red-dark">5</span>
+      <span class="badge bg-red-dark">{{ $total_item === 0 ? '' : $total_item }}</span>
     </a>
   </div>
 
@@ -15,19 +15,15 @@
     </div>
 
     <div class="row mb-2 mx-3">
-      <div class="col-12 ps-1 text-center">
-        <a href="#" class="btn btn-xxs mb-1 rounded-xl text-uppercase font-900 border-highlight color-highlight bg-theme">Sayuran</a>
-        <a href="#" class="btn btn-xxs mb-1 rounded-xl text-uppercase font-900 border-highlight color-highlight bg-theme">Ikan</a>
-        <a href="#" class="btn btn-xxs mb-1 rounded-xl text-uppercase font-900 border-highlight color-highlight bg-highlight">Sayuran</a>
-        <a href="#" class="btn btn-xxs mb-1 rounded-xl text-uppercase font-900 border-highlight color-highlight bg-theme">Sayuran</a>
-        <a href="#" class="btn btn-xxs mb-1 rounded-xl text-uppercase font-900 border-highlight color-highlight bg-theme">Sayuran</a>
-        <a href="#" class="btn btn-xxs mb-1 rounded-xl text-uppercase font-900 border-highlight color-highlight bg-theme">Sayuran</a>
-        <a href="#" class="btn btn-xxs mb-1 rounded-xl text-uppercase font-900 border-highlight color-highlight bg-theme">Sayuran</a>
+      <div class="col-12 ps-1">
+        @foreach($categories as $category)
+          <a href="#" class="btn btn-xxs mb-1 rounded-xl text-uppercase font-900 border-highlight color-highlight bg-theme">{{ $category->name }}</a>
+        @endforeach
       </div>
     </div>
 
     <div class="row mb-2 mx-3">
-      <div class="col-12 ps-1 text-center">
+      <div class="col-12 ps-1">
         <a href="#" class="btn btn-xxs mb-1 text-uppercase font-900 border-blue-dark color-blue-dark bg-theme">Promo</a>
         <a href="#" class="btn btn-xxs mb-1 text-uppercase font-900 border-blue-dark color-blue-dark bg-theme">Pilihan</a>
       </div>
@@ -48,5 +44,50 @@
         </div>
       </div>
     </div>
+
+    <div class="row mb-0 mx-3">
+      @foreach($menus as $i => $menu)
+        <div class="col-6 p-0 m-0">
+          <div class="card card-style m-1" wire:key="{{ $menu->id }}">
+            <div class="card card-style me-0 ms-0 mb-0" >
+              <a href="#" wire:click="selected({{ $menu }})" data-menu="menu-cart-edit-1"><img src="{{ asset($menu->image) }}"  class="rounded-sm shadow-xl img-fluid"></a>
+            </div>
+
+            <div class="content">
+              <a href="#" wire:click="selected({{ $menu }})" data-menu="menu-cart-edit-1">
+                <h5 class="mt-3" style="line-height: 0px">{{ $menu->name }}</h5>
+                <span class="color-green-dark font-10">
+                  @if($menu->in_stock)
+                    In Stock
+                  @else
+                    Tersedia: {{ $menu->stock }}
+                  @endif
+                </span>
+                <br>
+              </a>
+              @if($menu->special_price)
+                <span class=" font-10" style="line-height: 0px"><del>Rp {{ $menu->price }}</del> <span class="badge bg-green-light color-white">Hemat {{ $menu->discount }}%</span></span>
+              @endif
+
+              <h5 class=" color-highlight">Rp {{ $menu->special_price ?? $menu->price }} <span class="color-gray-dark font-12 font-500">/ {{ $menu->size_per_unit }} {{ $menu->unit->name }}</span></h5>
+
+              @if(checkBuyButton($menu->id, $items))
+                <a href="#" wire:click="increase({{ $menu }})" class="btn btn-xxs font-800 font-16 rounded-xl btn-full text-uppercase bg-highlight">BELI</a>
+              @else
+                <div class="align-self-center">
+                  <div class="stepper rounded-s float-start">
+                    <a href="#" class="stepper-sub" wire:click.prevent="decrease({{ $menu->id }})"><i class="fa fa-minus color-red-dark"></i></a>
+                    <input type="number" min="0" max="99" value="{{ getTotalBuyNumber($menu->id, $items) }}" readonly>
+                    <a href="#" class="stepper-add" wire:click.prevent="increase({{ $menu }})"><i class="fa fa-plus color-green-dark"></i></a>
+                  </div>
+                </div>
+              @endif
+            </div>
+          </div>
+        </div>
+      @endforeach
+    </div>
   </div>
+
+  @include('components.stickymobile.modal-menu')
 </div>
